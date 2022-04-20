20c5b2ddb55b7d9528c8c58dbef2ca74be820f8e
DawidMatura <72615110+dawimat009@users.noreply.github.com>
2022-04-20 10:19:26 +0200

    Initial commit

---
+++ src/main/java/com/example/jenkinsherokumatura/JenkinsherokumaturaApplication.java
@@ -0,0 +1,19 @@
+package com.example.jenkinsherokumatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class JenkinsherokumaturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsherokumaturaApplication.class, args);
+    }
+
+    @GetMapping
+    public String get(){
+        return "Hello Jenkins";
+    }
+}
+
